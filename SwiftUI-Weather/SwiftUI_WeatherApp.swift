87c5f2c1a8b56d6b5f9ee871287394d12b066ebc
Lucio Gutierrez <luciogutierrez1415@gmail.com>
2023-09-25 13:14:53 -0700

    Initial Commit

---
+++ SwiftUI-Weather/SwiftUI_WeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_WeatherApp.swift
+//  SwiftUI-Weather
+//
+//  Created by Lucio Gutierrez on 9/25/23.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_WeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
