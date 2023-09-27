21379f5b7ca96fa3e2177e8c22faf892ae0fd5eb
Lucio Gutierrez <luciogutierrez1415@gmail.com>
2023-09-27 09:59:22 -0700

    Created custom color as to not clash with the white text in front of it, created app!

--- SwiftUI-Weather/ContentView.swift
+++ SwiftUI-Weather/ContentView.swift
@@ -9,13 +9,49 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        ZStack {
+            BackgroundView(topColor: .blue, bottomColor: Color("lightBlue"))
+            VStack {
+                    CityTextView(cityName: "Cupertino, Ca")
+                 
+                MainWeatherStatusView(imageName: "cloud.sun.fill",
+                                      temperature: 76)
+                
+                Spacer()
+                
+                HStack(spacing: 20){
+                    WeatherDayView(dayOfWeek: "TUE",
+                                   imageName: "cloud.sun.fill",
+                                   temperature: 72)
+                    
+                    WeatherDayView(dayOfWeek: "WED",
+                                   imageName: "cloud.fog.fill",
+                                   temperature: 53)
+                    
+                    WeatherDayView(dayOfWeek: "THUR",
+                                   imageName: "wind.snow",
+                                   temperature: 56)
+                    
+                    WeatherDayView(dayOfWeek: "FRI",
+                                   imageName: "cloud.snow.fill",
+                                   temperature: 48)
+                    
+                    WeatherDayView(dayOfWeek: "SAT",
+                                   imageName: "cloud.bolt.rain.fill",
+                                   temperature: 62)
+                    
+                }
+                Spacer()
+                
+                Button {
+                    print("tapped")
+                }label: {
+                    WeatherButton(title: "Change Day Time",
+                                  textColor: .blue,
+                                  backgroundColor: .white)
+                }
+            }
         }
-        .padding()
     }
 }
 
@@ -24,3 +60,77 @@ struct ContentView_Previews: PreviewProvider {
         ContentView()
     }
 }
+
+struct WeatherDayView: View {
+    
+    var dayOfWeek: String
+    var imageName: String
+    var temperature: Int
+    
+    var body: some View {
+        VStack{
+            Text(dayOfWeek)
+                .font(.system(size: 16, weight: .medium, design: .default))
+                .foregroundColor(.white)
+            Image(systemName: imageName)
+                .renderingMode(.original)
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .frame(width: 40,height: 40)
+            
+            Text("\(temperature)")
+                .font(.system(size: 28,weight: .medium))
+                .foregroundColor(.white)
+            
+        }
+    }
+}
+
+struct BackgroundView: View {
+    
+    var topColor: Color
+    var bottomColor: Color
+    
+    var body: some View {
+        
+        LinearGradient(gradient: Gradient(colors: [topColor, bottomColor]),
+                       startPoint: .topLeading,
+                       endPoint: .bottomTrailing)
+        .ignoresSafeArea(.all)
+    }
+}
+
+struct CityTextView: View {
+    
+    var cityName: String
+    
+    var body: some View{
+        Text (cityName)
+            .font(.system(size: 32, weight: .medium, design: .default))
+            .foregroundColor(.white)
+            .padding()
+    }
+}
+
+struct MainWeatherStatusView: View{
+    
+    var imageName: String
+    var temperature: Int
+    
+    var body: some View{
+        VStack(spacing: 8){
+            Image(systemName: imageName)
+                .renderingMode(.original)
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .frame(width: 180,height: 180)
+            
+            Text("\(temperature)°")
+                .font(.system(size: 70,weight: .medium))
+                .foregroundColor(.white)
+            
+            
+        }
+    }
+}
+
---
+++ SwiftUI-Weather/WeatherButton.swift
@@ -0,0 +1,30 @@
+//
+//  WeatherButton.swift
+//  SwiftUI-Weather
+//
+//  Created by Lucio Gutierrez on 9/27/23.
+//
+
+import SwiftUI
+
+struct WeatherButton: View{
+    
+    var title: String
+    var textColor: Color
+    var backgroundColor: Color
+    
+    var body: some View{
+        Text(title)
+            .frame(width: 280, height: 50)
+            .background(backgroundColor)
+            .foregroundColor(textColor)
+            .font(.system(size: 20, weight: .bold, design: .default))
+            .cornerRadius(10)
+    }
+}
+
+struct WeatherButton_Previews: PreviewProvider {
+    static var previews: some View {
+        WeatherButton(title: "Change Day Time", textColor: .blue, backgroundColor: .white)
+    }
+}
